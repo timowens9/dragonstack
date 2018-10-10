e4be21786918e26f47a14c1e3c3105748e1ace45
Tim <timowens9@gmail.com>
2018-10-10 14:45:11 -0500

    Generation and dragon components add

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Generation from './components/Generation';
+import Dragon from './components/Dragon';
 
 
 render(
-    <div> Dragon Stack from React</div>,
+    <div>
+        <h2>Dragon Stack from React</h2>>
+            <Generation/>
+        <Dragon />
+    </div>,
     document.getElementById('root')
+
 );
